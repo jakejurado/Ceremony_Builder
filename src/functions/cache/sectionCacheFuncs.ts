956e7a1a49bf8da53480439d51f7a52f0042b94b
jakejurado <97064241+jakejurado@users.noreply.github.com>
2023-02-12 09:38:09 -0800

    fixed error with tsconfig: module to es6 and jsx to react

--- src/functions/cache/sectionCacheFuncs.ts
+++ src/functions/cache/sectionCacheFuncs.ts
@@ -1,11 +1,33 @@
-function fillCacheWithNewSections(cache: object, template: object, sendDups: boolean = false): object {
+
+// interface cacheType {
+//   [key: string]: object
+// }
+
+// function fillCacheWithNewSections(cache: cacheType, template: cacheType, sendDups: boolean = false): object {
+//   if (sendDups) return Object.assign(template, cache);
+//   const newCache: cacheType = { ...cache };
+//   for (const [key, obj] of Object.entries(template)) {
+//     const varname: string = key.split("~")[0];  
+//     if (!newCache.hasOwnProperty(varname)) newCache[varname] = obj;
+//   }
+//   return newCache;
+  
+// }
+
+
+// export { fillCacheWithNewSections };
+import React from 'react'
+
+function fillCacheWithNewSections(cache, template, sendDups = false) {
   if (sendDups) return Object.assign(template, cache);
-  const newCache: object = { ...cache };
+  const newCache = { ...cache };
   for (const [key, obj] of Object.entries(template)) {
-    const varname: string = key.split("~")[0];
+    const varname = key.split("~")[0];  
     if (!newCache.hasOwnProperty(varname)) newCache[varname] = obj;
   }
   return newCache;
+  
 }
+
 
 export { fillCacheWithNewSections };
