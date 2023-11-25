16ebb2f535596780c280059ef73b5b4c1b4af366
Jacob Jurado <jakejurado@gmail.com>
2023-11-24 19:09:04 -0700

    Moved sectionSelector titles/categories to a js file
    They were hard coded into the App.js page, so I moved it to another js page and imported it.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -13,7 +13,8 @@ import ErrorBoundary from "./ErrorBoundary";
 import Popup from './Popup';
 
 //Starting Data
-import {templateWed, templateElope } from '../server/files/database-script.js';
+import {templateWed, templateElope } from '../server/files/database-script';
+import  { allCategoryTitles } from '../server/files/selectorTitles';
 
 //helperFunctions
 import { updateSectionOrder } from "../functions/mainPage/dragdropFuncs";
@@ -77,78 +78,11 @@ function App() {
   });
 
   //holds data that needs to update state asynchronously
-const [fetchedData, setFetchedData] = useState(null);
+  const [fetchedData, setFetchedData] = useState(null);
 
 
-//holds all titles, varnames, and category in an object for all sections
-  //RESET TO EMPTY OBJECT FOR PRODUCTION
-  const [selectorTitles, setSelectorTitles] = useState({
-    "Basic Elements": {
-      "Giving Away": "giving_away",
-      "Opening Remarks: First Words": "opening_remarks1",
-      "Opening Remarks: Main Content": "opening_remarks2",
-      "Declaration of Intent": "declaration",
-      "Charge": "charge",
-      "Transition to Vows": "vows_symbolism",
-      "Vows": "vow_content",
-      "Rings Content": "ring_content",
-      "Ring Exchange": "ring_exchange",
-      "Pronouncement": "pronouncement",
-      "The Kiss": "kiss",
-      "Introduction": "introduction",
-    },
-    "Readings": { 
-      "Reading: Traditional": "reading_traditional",
-      "Reading: Humor": "reading_humor",
-      "Reading: Entertainment": "reading_entertainment",
-      "Reading: Scripture": "reading_scripture",
-    },
-    "Prayer": { 
-      "Prayer: Opening": "prayer_opening",
-      "Prayer: Wedding": "prayer_middle",
-      "Prayer: Benediction": "prayer_benediction", 
-    },
-    "Unity": { 
-      "Unity: Candle": "unity_candle",
-      "Unity: Sand": "unity_sand",
-      "Unity: Stone": "unity_stone",
-      "Unity: Wine Box:": "unity_winebox",
-      "Unity: Rose": "unity_rose",
-      "Unity: Tea Ceremony": "unity_tea",
-      "Unity: Hand Fasting": "unity_handfasting",
-      "Unity: Mead": "unity_mead",
-      "Unity: Cocktail": "unity_cocktail",
-      "Unity Canvas": "unity_canvas",
-      "Unity Glass": "unity_glass",
-      "Unity: Playdough": "unity_playdough",
-      "Unity: Beer": "unity_beer",
-      "Unity: Plant": "unity_plant",
-    },
-    "Religious": { 
-      "Arras": "arras",
-      "Lasso": "lasso",
-      "Veil": "veil",
-      "Veil & Cord": "veil_cord",
-      "God Knot": "god_knot",
-      "Breaking Glass": "breaking_glass",
-    },
-    "Including Others": { 
-      "Last Kiss": "last_kiss",
-      "Flowers For Mothers": "flowers_mothers",
-      "Gift To Parents": "gift_parents",
-      "Song": "song",
-      "Ring Warmer": "ring_warmer",
-      "Vows of Suppoert": "vows_of_support"
-    },
-    "Other Options": { 
-      "Blessing": "blessing",
-      "Deceased": "deceased",
-      "Turn Off Devices": "turnoff",
-      "License Singing": "license_sign",
-      "Custom": "custom"
-    },
-  });
-  // const [selectorTitles, setSelectorTitles] = useState({});
+  //holds all titles, varnames, and category in an object for all sections
+  const [selectorTitles, setSelectorTitles] = useState({...allCategoryTitles});
 
     //ref the content of the etite
   const domRef = useRef();
