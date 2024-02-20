67e3da41132fe625506d78c51c0b37f6f7d2d3e2
Jacob Jurado <jakejurado@gmail.com>
2024-02-20 05:33:45 -0800

    AI / design popup request
    finished the design of the ai popup request.

--- src/Components/PopupAISubmitScreen.jsx
+++ src/Components/PopupAISubmitScreen.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useContext, useCallback} from "react";
+import { GlobalContext} from "./App";
 
-//909135
 
 function PopupAISubmitScreen({cardContent}){
+  const {popupDispatch} = useContext(GlobalContext)
+
+  //connects with parent state that displays the popup and removes it.
+  const handleBackgroundClick = useCallback(() =>{
+    popupDispatch({type: null, box: null});
+  }, [])
+
   return(
     <>
 
@@ -10,42 +17,45 @@ function PopupAISubmitScreen({cardContent}){
         <p>{cardContent}</p>
       </div>
 
+      <div id='aiInstructions'>
+        <p><i>Choose the options for the AI to edit the content</i></p>
+      </div>
+
       <div id='aiOptions'>
 
-        <div class="aiOption">
+        <div className="aiOption">
           <h3>Length</h3>
-          <div class="aiOptionButtons length-btn-group">
+          <div className="aiOptionButtons length-btn-group">
             <input type="radio" id="shorter" name="length" value="Shorter" class="hidden-radio"/>
-            <label class="option-btn" for="shorter">Shorter</label>
+            <label className="option-btn" htmlFor="shorter">Shorter</label>
 
             <input type="radio" id="reword-length" name="length" value="Rephrase" class="hidden-radio"/>
-            <label class="option-btn" for="reword-length">Rephrase</label>
+            <label className="option-btn" htmlFor="reword-length">Rephrase</label>
 
             <input type="radio" id="longer" name="length" value="Longer" class="hidden-radio"/>
-            <label class="option-btn" for="longer">Longer</label>
+            <label className="option-btn" htmlFor="longer">Longer</label>
           </div>
         </div>
 
-        <div class="aiOption">
+        <div className="aiOption">
           <h3>Tone</h3>
-          <div class="aiOptionButtons tone-btn-group">
+          <div className="aiOptionButtons tone-btn-group">
             <input type="radio" id="informal" name="tone" value="Informal" class="hidden-radio"/>
-            <label class="option-btn" for="informal">Informal</label>
+            <label className="option-btn" htmlFor="informal">Informal</label>
 
             <input type="radio" id="same-tone" name="tone" value="Same" class="hidden-radio"/>
-            <label class="option-btn" for="same-tone">Same</label>
+            <label className="option-btn" htmlFor="same-tone">Same</label>
 
             <input type="radio" id="formal" name="tone" value="Formal" class="hidden-radio"/>
-            <label class="option-btn" for="formal">Formal</label>
+            <label className="option-btn" htmlFor="formal">Formal</label>
           </div>
         </div>
-
 
       </div>
 
       <div id='aiButtons'>
         <button>Submit</button>
-        <button>Cancel</button>
+        <button onClick={handleBackgroundClick}>Cancel</button>
       </div>
 
     </>
